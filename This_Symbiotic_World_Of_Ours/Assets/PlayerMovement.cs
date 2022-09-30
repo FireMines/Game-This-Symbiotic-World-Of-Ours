08b81e952ed24b5d2bdd9ed5ce8a35c85be5f4fe
Lars Blütecher Holter <larsbho@stud.ntnu.no>
2022-09-30 09:51:05 +0200

    Previous speed to fast, slow down! #3

--- This_Symbiotic_World_Of_Ours/Assets/PlayerMovement.cs
+++ This_Symbiotic_World_Of_Ours/Assets/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
 
     public CharacterController2D controller;
 
-    public float runSpeed = 4000f;
+    public float runSpeed = 50f;
 
     float horizontalMove = 0f;
     bool jump = false;
