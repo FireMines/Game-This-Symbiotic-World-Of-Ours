e938a079c0b258ef30ebd3ce5bf1cf737130ab3c
Lars Blütecher Holter <larsbho@stud.ntnu.no>
2022-10-26 00:37:49 +0200

    [#8] Added more assets, created start of a transition between levels

---
+++ This_Symbiotic_World_Of_Ours/Assets/CrossfadeScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossfadeScript : MonoBehaviour
+{
+    private Animator animationController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animationController = GetComponent<Animator>();
+        if (animationController == null) Debug.Log("Error! Crossfade doesn't have an animation controller.");
+    }
+
+    public void StartCrossfade()
+    {
+        //print();
+        animationController.SetTrigger("Start");
+    }
+}
--- This_Symbiotic_World_Of_Ours/Assets/PlayerMovement.cs
+++ This_Symbiotic_World_Of_Ours/Assets/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -39,5 +40,4 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
     }
-
 }
