e12900beac0124f3ceac68c754d65f7667c438ea
sarahseif <sarahsei@stud.ntnu.no>
2022-10-25 21:33:01 +0200

    player character can "grab" objects such as rocks and move them in any direction

--- This_Symbiotic_World_Of_Ours/Assets/ObstaclePush.cs
+++ This_Symbiotic_World_Of_Ours/Assets/ObstaclePush.cs
@@ -5,26 +5,27 @@ using UnityEngine;
 public class ObstaclePush : MonoBehaviour
 {
     [SerializeField]
-    private float pushForce;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform grabDetect;
+    public Transform boxHolder;
+    public float rayDist;
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Rigidbody2D rigidbody = collision.rigidbody;
-
-        if(rigidbody != null && rigidbody.tag != "Pushable" && rigidbody.tag !="Player") 
-        {   
-            rigidbody.constraints = RigidbodyConstraints2D.FreezePosition;        
+    void Update(){
+        RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale, rayDist);
+        if(grabCheck.collider != null && grabCheck.collider.gameObject.GetComponent<Rigidbody2D>()!= null && grabCheck.collider.tag != "Pushable" ){
+            grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+        }
+        if(grabCheck.collider != null && grabCheck.collider.tag == "Pushable" ){
+            if(Input.GetKey(KeyCode.G)){
+                grabCheck.collider.gameObject.transform.parent = boxHolder;
+                grabCheck.collider.gameObject.transform.position = boxHolder.position;
+                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().mass=0.00001F;
+            }else{
+                grabCheck.collider.gameObject.transform.parent = null;
+                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+                
+                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().mass=1F;//grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            }
         }
     }
 }
