1dbd6fa38031dd60a74922d685e353c3eb65db81
sarahseif <sarahsei@stud.ntnu.no>
2022-11-01 17:28:35 +0100

    [#46] enemies automatically move from left to right

--- This_Symbiotic_World_Of_Ours/Assets/Enemy.cs
+++ This_Symbiotic_World_Of_Ours/Assets/Enemy.cs
@@ -4,9 +4,47 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public int damage;
-    public PlayerHealth playerHealth;
+    [SerializeField] private int damage;
+    [SerializeField] private PlayerHealth playerHealth;
+    [SerializeField] private SpriteRenderer _enemySprite;
+    private const int distance = 500;
+    private float XPosition;
+    private int movementIndex = 1;
+    private int numberOfCalls;
+    private bool counterUp = true; // position = position +1 if true, -1 if false   
     
+    private void Start(){
+        //get the enemies starting position as the "main" position the enemy moves from
+        XPosition = transform.position.x;
+        numberOfCalls = distance*100;
+    }
+    private void Update(){
+        enemyMovement();
+    }
+
+    private void enemyMovement(){
+        //move the enemy a set distance from the starting point and then back(e.g. +/-5)
+        //TODO: add player detection
+        float newXPosition = 0f;
+        if(counterUp){
+            XPosition = XPosition+0.01f;
+            movementIndex++;
+        }else{
+            XPosition = XPosition-0.01f;
+            movementIndex--;
+        }
+        transform.position = new Vector3(XPosition, transform.position.y, transform.position.z);
+        
+        switch(movementIndex){
+            case 0:
+                counterUp=true;
+                break;
+            case distance:
+                counterUp=false;
+                break;
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.tag=="Player"){
             playerHealth.takeDamage(damage);
--- This_Symbiotic_World_Of_Ours/Assets/ObstaclePush.cs
+++ This_Symbiotic_World_Of_Ours/Assets/ObstaclePush.cs
@@ -12,18 +12,20 @@ public class ObstaclePush : MonoBehaviour
 
     void Update(){
         RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale, rayDist);
-        if(grabCheck.collider != null && grabCheck.collider.gameObject.GetComponent<Rigidbody2D>()!= null && grabCheck.collider.tag != "Pushable" ){
-            grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
-        }
-        if(grabCheck.collider != null && grabCheck.collider.tag == "Pushable" ){
-            if(Input.GetKey(KeyCode.E)){
-                grabCheck.collider.gameObject.transform.parent = boxHolder;
-                grabCheck.collider.gameObject.transform.position = boxHolder.position;
-                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().mass=0.00001F;
-            }else{
-                grabCheck.collider.gameObject.transform.parent = null;
-                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().mass=10F;
-                }
+        if(grabCheck.collider != null && grabCheck.collider.tag!="Enemy"){
+            if(grabCheck.collider != null && grabCheck.collider.gameObject.GetComponent<Rigidbody2D>()!= null && grabCheck.collider.tag != "Pushable" ){
+                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+            }
+            if(grabCheck.collider != null && grabCheck.collider.tag == "Pushable" ){
+                if(Input.GetKey(KeyCode.E)){
+                    grabCheck.collider.gameObject.transform.parent = boxHolder;
+                    grabCheck.collider.gameObject.transform.position = boxHolder.position;
+                    grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().mass=0.00001F;
+                }else{
+                    grabCheck.collider.gameObject.transform.parent = null;
+                    grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().mass=10F;
+                    }
+            }
         }
     }
 }
