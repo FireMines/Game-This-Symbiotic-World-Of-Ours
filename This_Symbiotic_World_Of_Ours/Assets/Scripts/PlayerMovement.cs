9a94afe88daaec294147e5c6377c315b7d07b669
sarahseif <sarahsei@stud.ntnu.no>
2022-11-09 22:49:23 +0100

    changed swimming up a little

--- This_Symbiotic_World_Of_Ours/Assets/Scripts/PlayerMovement.cs
+++ This_Symbiotic_World_Of_Ours/Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,7 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 50f;
 
     float horizontalMove = 0f;
+    float verticalMove = 0f;
     bool jump = false;
     bool crouch = false;
 
@@ -26,6 +27,7 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            verticalMove = 40f;
         }
 
         if (Input.GetButtonDown("Crouch"))
@@ -34,13 +36,16 @@ public class PlayerMovement : MonoBehaviour
         } else if (Input.GetButtonUp("Crouch"))
         {
             crouch = false;
+            verticalMove=-40f;
         }
+
+        print(verticalMove);
     }
 
     void FixedUpdate ()
     {
         // Move our character
-        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, verticalMove, crouch, jump);
         jump = false;
     }
 }
