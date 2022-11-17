48853177af480746a1ddede407c5d9eb002261ba
sarahseif <sarahsei@stud.ntnu.no>
2022-11-17 16:22:23 +0100

    no issue number, fix push and pull: it's still glitchy and buggy but it works so it's a start

--- This_Symbiotic_World_Of_Ours/Assets/Scripts/ObstaclePush.cs
+++ This_Symbiotic_World_Of_Ours/Assets/Scripts/ObstaclePush.cs
@@ -14,37 +14,47 @@ public class ObstaclePush : MonoBehaviour
         if(pushObject==null){
             pushObject = col.gameObject;
         }
-    }
-
-    void Update(){
         
         if(pushObject != null && pushObject.tag!="Enemy"){//check that collided game object is not an enemy!! player should not be able to pull and push them
             if(pushObject != null && pushObject.GetComponent<Rigidbody2D>() != null && pushObject.tag != "Pushable" ){
                 //freeze it's position if it's not pushable
                 pushObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+                pushObject=null;
             }
+        }else{
+            pushObject=null;
+        }
+    }
+
+    void Update(){
             if(pushObject != null && pushObject.tag == "Pushable" ){
                 if(Input.GetKey(KeyCode.E)){
+                    pushObject.GetComponent<Rigidbody2D>().mass=0f;
                     //if e is pressed, push or pull the object
                     pushObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
-                    //if key right is pressed -> move towards position + speed
-                    //if key left is pressed -> move towards position - speed
+                    //for it to work properly, now: if player is walking towards rock -> push
+                    //if player is walking away from rock -> pull
                     if(Input.GetKey("right") || Input.GetKey(KeyCode.D)){
+                        //if player pos > rock pos:
                         print("right");
-                        Vector2 newPos = new Vector2(pushObject.transform.position.x + 2f, pushObject.transform.position.y);
-                        pushObject.transform.position = Vector2.MoveTowards(pushObject.transform.position, newPos , playerSpeed / 100f);
+                        if(gameObject.transform.position.x>pushObject.transform.position.x){
+                            pushObject.transform.position = Vector2.MoveTowards(pushObject.transform.position, gameObject.transform.position , playerSpeed / 400f);
+                        }
+                        //else nothing, rock gets pushed normally
                     }
                     if(Input.GetKey("left") || Input.GetKey(KeyCode.A)){
                         print("left");
-                        Vector2 newPos = new Vector2(pushObject.transform.position.x - 2f, pushObject.transform.position.y);
-                        pushObject.transform.position = Vector2.MoveTowards(pushObject.transform.position, newPos , playerSpeed / 100f);
+                        //if player pos < rock pos:
+                        if(gameObject.transform.position.x<pushObject.transform.position.x){
+                            pushObject.transform.position = Vector2.MoveTowards(pushObject.transform.position, gameObject.transform.position , playerSpeed / 400f);
+                        }
                     }
                     
                 }else{
+                    //if you want object to stop when e is not pressed anymore: else just comment it out
                     pushObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
                     pushObject = null;
                 }
             }else{pushObject = null;}
-        }else{pushObject = null;}
     }
 }
