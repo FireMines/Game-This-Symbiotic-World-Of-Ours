b93495ab5f352f607bf5b02efb9b521e32e2e5b1
Lars Blütecher Holter <larsbho@stud.ntnu.no>
2022-11-23 15:24:05 +0100

    [#27, #53] Added an ending to game after talking with world spirit, adjusted rain, added friendlies to earth level and added signs to home base

--- This_Symbiotic_World_Of_Ours/Assets/Scripts/StopRain.cs
+++ This_Symbiotic_World_Of_Ours/Assets/Scripts/StopRain.cs
@@ -6,16 +6,21 @@ public class StopRain : MonoBehaviour
 {
 
     private CharacterController2D player;
+    Vector3 cloudPos;
 
     // Start is called before the first frame update
     void Start()
     {
+        cloudPos = this.transform.position;
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        cloudPos.x = player.transform.position.x + 10f;
+        cloudPos.y = player.transform.position.y + 30f;
+        this.transform.position = cloudPos;
         player.GetTotalOrbAmount();
         ParticleSystem part = GetComponent<ParticleSystem>();
 
