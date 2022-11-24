e5ff3e87a6cd18285b63afe7d4e4aa48f64261ac
Lars Blütecher Holter <larsbho@stud.ntnu.no>
2022-11-24 02:11:04 +0100

    Rain now starts when collecting all orbs in water level and stops when collecting all orbs in earth level

--- This_Symbiotic_World_Of_Ours/Assets/Scripts/StopRain.cs
+++ This_Symbiotic_World_Of_Ours/Assets/Scripts/StopRain.cs
@@ -1,19 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StopRain : MonoBehaviour
 {
 
     private CharacterController2D player;
     Vector3 cloudPos;
+    Scene currentScene = SceneManager.GetActiveScene();
+    ParticleSystem part; 
 
     // Start is called before the first frame update
     void Start()
     {
+        part = GetComponent<ParticleSystem>();
+
+        // Unload all other scenes that arent DoNotUnload
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            // Skips unloading if its in the DoNotUnload Scene
+            if (SceneManager.GetSceneAt(i).name == "FirstWaterLevelScene") part.Stop();
+
+            if (SceneManager.GetSceneAt(i).name == "FirstEarthLevelScene") part.Play();
+
+        }
+
+
         cloudPos = this.transform.position;
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController2D>();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -22,11 +39,15 @@ public class StopRain : MonoBehaviour
         cloudPos.y = player.transform.position.y + 30f;
         this.transform.position = cloudPos;
         player.GetTotalOrbAmount();
-        ParticleSystem part = GetComponent<ParticleSystem>();
 
-        int orbsCollected = player.GetOrbAmount(OrbController.Element.Water);
+        int waterOrbsCollected = player.GetOrbAmount(OrbController.Element.Water);
 
-        if (orbsCollected >= 3)
+        if (waterOrbsCollected >= 3)
             part.Stop();
+
+        int earthOrbsCollected = player.GetOrbAmount(OrbController.Element.Earth);
+        print(earthOrbsCollected);
+        if (earthOrbsCollected >= 3)
+            part.Play();
     }
 }
