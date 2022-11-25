50bb9dff7c9b6ab5a9132fda0df0f5d9b2fede53
Lars Blütecher Holter <larsbho@stud.ntnu.no>
2022-11-25 01:29:05 +0100

    [#52] Healthbar for player now fully implemented

---
+++ This_Symbiotic_World_Of_Ours/Assets/Scripts/DisplayPlayerHealth.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DisplayPlayerHealth : MonoBehaviour
+{
+    public Image LifeCell;
+    public Image deathScreen;
+    public Sprite test;
+    public Canvas healthCanvas;
+
+    private HealthController characterInfo;    // Character info
+
+
+    // All Life Cells
+    [SerializeField] private List<Image> LifeCells = new List<Image>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RectTransform LifeCell = GetComponent<RectTransform>();
+        LifeCell.anchoredPosition = new Vector2(LifeCell.anchoredPosition.x, LifeCell.anchoredPosition.y + 20);
+        characterInfo = GameObject.FindGameObjectWithTag("Player").GetComponent<HealthController>();
+
+        //LifeCell = Image.FindObjectOfType();
+        //UpdateHealthbar();
+
+        createHealthImageBasedOnHP();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateHealthbar();
+        //createHealthImageBasedOnHP();
+    }
+
+
+    /// <summary>
+    /// Creates the amount of Lifecells the player have on screen display
+    /// </summary>
+    public void createHealthImageBasedOnHP()
+    {
+        Vector2 lifecellPos;
+        lifecellPos.x = Screen.width/3;
+        lifecellPos.y = 50;
+        for (int i = 0; i < characterInfo.health; i++)
+        {
+            GameObject NewObj = new GameObject();
+            Image LifeCell = NewObj.AddComponent<Image>();
+            print("Kebab " + LifeCell.gameObject.GetComponent<RectTransform>().position);
+            LifeCell.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(20, 20);
+            LifeCell.gameObject.GetComponent<RectTransform>().position = lifecellPos;
+            LifeCell.name = "Lifecell_" + i;
+            LifeCell.transform.parent = healthCanvas.transform;
+            lifecellPos.x += 50;
+            LifeCell.sprite = test;
+            LifeCells.Add(LifeCell);
+        }
+    }
+
+
+    /// <summary>
+    /// Updates how much health is left on the player health bar
+    /// </summary>
+    public void UpdateHealthbar()
+    {
+        //print("yoyoyo");
+        /*
+        if (characterInfo.health <= 0)
+        {
+            //Vector4 screenColor;
+            //screenColor.Set(0, 0, 0, 255);
+            //screenColor.
+            // Attempts to fade screen to black when dead
+            deathScreen.color = new Color(0f, 0f, 0f, Mathf.PingPong(Time.time, 1));
+        }*/
+
+        //if (!displayHP) return;
+
+        for (int i = 0; i < LifeCells.Count; i++)
+        {
+            if (i < characterInfo.health)
+            {
+                //print("i: " + i + "\nhealth: " + characterInfo.getHealth());
+                //LifeCells[i].enabled = true;
+                LifeCells[i].color = Color.green;
+            }
+            else
+            {
+                //LifeCells[i].enabled = false;
+                LifeCells[i].color = Color.black;
+            }
+        }
+    }
+
+}
--- This_Symbiotic_World_Of_Ours/Assets/Scripts/HealthController.cs
+++ This_Symbiotic_World_Of_Ours/Assets/Scripts/HealthController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class HealthController : MonoBehaviour
 {
@@ -9,65 +8,9 @@ public class HealthController : MonoBehaviour
     public bool     displayHP = false;      // Show health if mob is player character
     private Color   colour;
 
-    public Image LifeCell;
-    public Image deathScreen;
-
-    // All Life Cells
-    [SerializeField] private List<Image> LifeCells = new List<Image>();
-
     private void Start()
     {
-        //createHealthImageBasedOnHP();
-        //UpdateHealthbar();
-    }
 
-
-    /// <summary>
-    /// Updates how much health is left on the player health bar
-    /// </summary>
-    public void UpdateHealthbar()
-    {
-        if(health <= 0)
-        {
-            //Vector4 screenColor;
-            //screenColor.Set(0, 0, 0, 255);
-            //screenColor.
-            // Attempts to fade screen to black when dead
-            deathScreen.color = new Color(0f, 0f, 0f, Mathf.PingPong(Time.time, 1));
-        }
-
-        if (!displayHP) return;
-
-        for (int i = 0; i < LifeCells.Count; i++)
-        {
-            if (i < health)
-            {
-                LifeCells[i].enabled = true;
-                LifeCells[i].color = Color.green;
-            } else
-            {
-                LifeCells[i].enabled = false;
-                LifeCells[i].color = Color.black;
-            }
-        }
-    }
-
-
-    /// <summary>
-    /// Creates the amount of Lifecells the player have on screen display
-    /// </summary>
-    public void createHealthImageBasedOnHP ()
-    {
-        Vector2 lifecellPos;
-        lifecellPos.x = -150;
-        lifecellPos.y = 50;
-        for(int i = 0; i < health; i++)
-        {
-            lifecellPos.x += 100;
-            LifeCell.gameObject.transform.position = lifecellPos;
-            LifeCells.Add(LifeCell);
-
-        }
     }
 
 
