fd863aa61fa1e2a1d72ac1910fe477e8ba78c989
Lars Blütecher Holter <larsbho@stud.ntnu.no>
2022-12-11 02:03:34 +0100

    [#54, #55, #74] Reworked the whole dialog system to be more universal and better generally.

--- This_Symbiotic_World_Of_Ours/Assets/Scripts/Dialogue.cs
+++ This_Symbiotic_World_Of_Ours/Assets/Scripts/Dialogue.cs
@@ -1,64 +1,123 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class Dialogue : MonoBehaviour
 {
-    public TextMeshProUGUI textComponent;
-    public string[] lines;
-    public float textSpeed;
+    [Header("Setup")]
+    public GameObject DialoguePanel;
+    public GameObject ContinueButton;
+    public TextMeshProUGUI DialogueText;
+    public TextMeshProUGUI NameOfTalker;
+    public GameObject image;
+    public float WordSpeed;
 
+    private string[] dialogue;
     private int index;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        textComponent.text = string.Empty;
-        StartDialogue();
-    }
+    private Action<string> onLineListener = null;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (dialogue == null) return;
+        if (DialogueText == null) return;
+
+
+        if (DialogueText.text == dialogue[index])
         {
-            if(textComponent.text == lines[index])
-            {
-                NextLine();
-            } else
-            {
-                StopAllCoroutines();
-                textComponent.text = lines[index];
-            }
+            ContinueButton.SetActive(true);
         }
     }
 
-    void StartDialogue()
+    public void zeroText()
     {
+        DialogueText.text = "";
         index = 0;
-        StartCoroutine(TypeLine());
+        DialoguePanel.SetActive(false);
     }
 
-    IEnumerator TypeLine()
+    public IEnumerator Typing()
     {
-        foreach(char c in lines[index].ToCharArray())
+        foreach (char letter in dialogue[index].ToCharArray())
         {
-            textComponent.text += c;
-            yield return new WaitForSeconds(textSpeed);
+            DialogueText.text += letter;
+            yield return new WaitForSeconds(WordSpeed);
         }
     }
 
-    void NextLine()
+    public void NextLine()
     {
-        if(index < lines.Length - 1)
+        ContinueButton.SetActive(false);
+
+        if (index < dialogue.Length - 1)
         {
             index++;
-            textComponent.text = string.Empty;
-            StartCoroutine(TypeLine());
-        } else
+            if(onLineListener != null) onLineListener(dialogue[index]);
+            DialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
         {
-            gameObject.SetActive(true);
+            zeroText();
+            if (onLineListener != null) onLineListener("");
+            
         }
     }
+
+
+    public void setOnLineListener(Action<string> callback = null)
+    {
+        onLineListener = callback;
+    }
+
+    /// <summary>
+    /// Toggles the text to either open and start typing or close the text window
+    /// </summary>
+    public void ToggleText()
+    {
+        if (DialoguePanel.activeInHierarchy)
+        {
+            zeroText();
+        }
+        else
+        {
+            DialoguePanel.SetActive(true);
+            StartCoroutine(Typing());
+        }
+    }
+
+
+    /// <summary>
+    /// Sets the dialogue
+    /// </summary>
+    /// <param name="sentences">Dialogue from a particular element</param>
+    public void setDialogue(string[] sentences)
+    {
+        dialogue = sentences;
+    }
+
+
+    /// <summary>
+    /// Sets the name of element you have a dialog with
+    /// </summary>
+    /// <param name="name">Name of element you are interacting with</param>
+    public void setNameOfTalker(string name)
+    {
+        NameOfTalker.SetText(name);
+    }
+
+    
+    /// <summary>
+    /// Set the image of the element you have a dialog with
+    /// </summary>
+    /// <param name="picture">Image of element you are interacting with</param>
+    public void setImage(Sprite picture)
+    {
+        image.GetComponent<Image>().sprite = picture;
+    }
+
 }
